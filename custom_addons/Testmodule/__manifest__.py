92e9136183b833c505857ac1e907a181d36fd4e6
anastasiia <anastasieshcherbyna@gmail.com>
2025-01-17 12:32:49 +0200

    Added Testmodule

---
+++ custom_addons/Testmodule/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'My Test Module',
+    'version': '1.0',
+    'author': 'Ваше ім’я',
+    'category': 'Tools',
+    'summary': 'Test module for learning purposes',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
